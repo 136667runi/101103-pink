d1b2264da5740f168681867c337baf791f834817
alpden550 <alpden550@gmail.com>
2016-10-06 18:29:07 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ var menu = document.querySelector('.main-menu');
 
 var headerNav = document.querySelector('.page-header__nav');
 var container = document.querySelector('.container');
+var apps = document.querySelector('.page-header__apps');
 
 function ready() {
   menu.classList.remove('main-menu--open');
   button.classList.remove('page-header__menu--open');
   headerNav.classList.remove('page-header__nav--menu-open');
   container.classList.remove('container--main');
+  apps.classList.remove('page-header__apps--menu-open');
 }
 document.addEventListener('DOMContentLoaded', ready);
 
@@ -20,9 +22,11 @@ button.addEventListener('click', function (event) {
   if (menu.classList.contains('main-menu--open')) {
     headerNav.classList.add('page-header__nav--menu-open');
     container.classList.add('container--main');
+    apps.classList.add('page-header__apps--menu-open');
   } else {
     headerNav.classList.remove('page-header__nav--menu-open');
     container.classList.remove('container--main');
+    apps.classList.remove('page-header__apps--menu-open');
   }
 
 })
